cad287e1a4916b65181dff08e6acdfe476623a73
SVNRiS3 <krzy.driwa@gmail.com>
2024-05-23 14:38:03 +0200

    Add constraints validation

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ class Validator {
   constructor(form) {
     this.form = document.querySelector(form);
     this.inputs = {
-      emailInput: form.querySelector('input[type="email"]'),
-      zipCodeInput: form.querySelector('input[name="zip"]'),
-      pwdInput: form.querySelector('input[name="password"]'),
-      pwdConfirmationInput: form.querySelector(
+      emailInput: this.form.querySelector('input[type="email"]'),
+      zipCodeInput: this.form.querySelector('input[name="zip"]'),
+      pwdInput: this.form.querySelector('input[name="password"]'),
+      pwdConfirmationInput: this.form.querySelector(
         'input[name="password-confirmation"]'
       ),
     };
-    this.submitButton = form.querySelector('button');
+    this.submitButton = this.form.querySelector('button');
+  }
+
+  checkPasswordMatching() {
+    return (
+      this.inputs.pwdInput.textContent ===
+      this.inputs.pwdConfirmationInput.textContent
+    );
   }
 
   validate(input) {
@@ -20,15 +27,31 @@ class Validator {
       return true;
     }
     if (input.validity.patternMismatch) {
-      // error
+      errorElement.textContent = 'zip-code not in xx-xx format';
+    } else if (input.validity.typeMismatch) {
+      errorElement.textContent = 'e-mail address invalid';
+    } else if (input.validity.valueMissing) {
+      errorElement.textContent = "field can't be empty";
+    } else if (!checkPasswordMatching()) {
+      errorElement.textContent =
+        'maximum password length of 32 symbols exceeded';
+    } else if (input.validity.tooLong) {
+      errorElement.textContent =
+        'maximum password length of 32 symbols exceeded';
+    } else if (input.validity.tooShort) {
+      errorElement.textContent =
+        'minimum password length of 8 symbols not reached';
     }
     errorElement.className = 'error active';
     return false;
   }
 
   addValidation() {
-    Object.keys(this.inputs).forEach((input) => {
-      input.addEventListener('input', validate(input));
+    Object.values(this.inputs).forEach((input) => {
+      input.addEventListener('input', () => this.validate(input));
     });
   }
 }
+
+const validator = new Validator('form');
+validator.addValidation();
