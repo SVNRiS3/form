a9c7bbed9aeeac2ffb729198b30fea7511e078c4
SVNRiS3 <krzy.driwa@gmail.com>
2024-05-23 13:59:11 +0200

    Select relevant DOM elements

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 class Validator {
   constructor(form) {
-    this.form = form;
+    this.form = document.querySelector(form);
+    this.emailInput = form.querySelector('input[type="email"]');
+    this.zipCodeInput = form.querySelector('input[name="zip"]');
+    this.pwdInput = form.querySelector('input[name="password"]');
+    this.pwdConfirmationInput = form.querySelector(
+      'input[name="password-confirmation"]'
+    );
+    this.submitButton = form.querySelector('button');
   }
 }
