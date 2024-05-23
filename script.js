a2b25187888e1f3c0d2849da065156448177423f
SVNRiS3 <krzy.driwa@gmail.com>
2024-05-23 14:01:23 +0200

    Group inputs

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 class Validator {
   constructor(form) {
     this.form = document.querySelector(form);
-    this.emailInput = form.querySelector('input[type="email"]');
-    this.zipCodeInput = form.querySelector('input[name="zip"]');
-    this.pwdInput = form.querySelector('input[name="password"]');
-    this.pwdConfirmationInput = form.querySelector(
-      'input[name="password-confirmation"]'
-    );
+    this.inputs = {
+      emailInput: form.querySelector('input[type="email"]'),
+      zipCodeInput: form.querySelector('input[name="zip"]'),
+      pwdInput: form.querySelector('input[name="password"]'),
+      pwdConfirmationInput: form.querySelector(
+        'input[name="password-confirmation"]'
+      ),
+    };
     this.submitButton = form.querySelector('button');
   }
+
+  validate() {}
 }
